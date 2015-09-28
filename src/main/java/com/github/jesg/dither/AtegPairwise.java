ebefd33c582e38f93b99354ffb0e811cd619714a
jesg <gowanjason@gmail.com>
2015-09-28 16:35:26 -0500

    remove Pair inner class from ategpairwise

--- src/main/java/com/github/jesg/dither/AtegPairwise.java
+++ src/main/java/com/github/jesg/dither/AtegPairwise.java
@@ -250,40 +250,4 @@ outer:
         }
         return flag;
     }
-
-    private class Pair {
-        final int i;
-        final int j;
-
-        Pair(final int i, final int j) {
-            this.i = i;
-            this.j = j;
-        }
-
-
-        @Override
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + i;
-            result = prime * result + j;
-            return result;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null)
-                return false;
-            if (getClass() != obj.getClass())
-                return false;
-            Pair other = (Pair) obj;
-            if (j != other.j)
-                return false;
-            if (i != other.i)
-                return false;
-            return true;
-        }
-    }
 }
